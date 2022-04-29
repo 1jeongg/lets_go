da97de49dbe117ea4aafa47c03751cd9a95b7a5c
1jeongg <leena09123@naver.com>
2022-04-29 20:26:49 +0900

    2nd commit

--- app/src/main/java/com/example/lets_go/MainActivity.java
+++ app/src/main/java/com/example/lets_go/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.lets_go;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn_find_cafe_map, btn_find_cafe, btn_register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        btn_find_cafe_map = findViewById(R.id.btn_find_cafe_map);
+        btn_find_cafe = findViewById(R.id.btn_find_cafe);
+        btn_register = findViewById(R.id.btn_register);
+
+        btn_find_cafe_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CafeMapActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn_find_cafe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CafeListActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterCafeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/lets_go/RegisterCafeActivity.java
@@ -0,0 +1,30 @@
+package com.example.lets_go;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+
+public class RegisterCafeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_cafe);
+
+        final CheckBox cb1 = (CheckBox)findViewById(R.id.checkbox1);
+        final CheckBox cb2 = (CheckBox)findViewById(R.id.checkbox2);
+        final CheckBox cb3 = (CheckBox)findViewById(R.id.checkbox3);
+        final CheckBox cb4 = (CheckBox)findViewById(R.id.checkbox4);
+        final CheckBox cb5 = (CheckBox)findViewById(R.id.checkbox5);
+        final CheckBox cb6 = (CheckBox)findViewById(R.id.checkbox6);
+        final CheckBox cb7 = (CheckBox)findViewById(R.id.checkbox7);
+        final CheckBox cb8 = (CheckBox)findViewById(R.id.checkbox8);
+        final CheckBox cb9 = (CheckBox)findViewById(R.id.checkbox9);
+        final CheckBox cb10 = (CheckBox)findViewById(R.id.checkbox10);
+        final CheckBox cb11 = (CheckBox)findViewById(R.id.checkbox11);
+
+
+    }
+}
